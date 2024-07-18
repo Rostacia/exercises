277abf5fddc0b70677f80db4d40851304dd015a4
Rostacia <rostacia_lewis@hotmail.com>
2024-07-18 15:34:12 -0400

    Modulizing code and testing it

---
+++ rectangles/overlap_v1.py
@@ -0,0 +1,28 @@
+import sys
+
+def main(infile, outfile):
+    # build rectangle struct
+    dict = {}
+    for line in infile:
+        name, *coords = line.split()
+        dict[name] = [int(c) for c in coords]
+
+    for red_name, red_coords in dict.items():
+        for blue_name, blue_coords in dict.items():
+            # check if rects overlap
+            result = '1'
+
+            red_lo_x, red_lo_y, red_hi_x, red_hi_y = red_coords
+            blue_lo_x, blue_lo_y, blue_hi_x, blue_hi_y = blue_coords
+
+            if (red_lo_x >= blue_hi_x) or (red_hi_x <= blue_lo_x) or \
+                    (red_lo_y >= blue_hi_x) or (red_hi_y <= blue_lo_y):
+                result = '0'
+
+            outfile.write(result + '\t')
+        outfile.write('\n')
+    
+if __name__ == "__main__":
+   with open(sys.argv[1], encoding='utf-8') as infile, \
+           open(sys.argv[2], 'w', encoding='utf-8') as outfile:
+       main(infile, outfile)    
---
+++ rectangles/text_overlap.py
@@ -0,0 +1,4 @@
+import overlap_v1 as overlap  # change version as needed
+
+def test_import():
+    pass
